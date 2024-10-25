5a07f3ce41bf7cd79440930f346d8a5f7031bdc1
mr21sk <krishnapatil12.k4@gmail.com>
2024-10-26 00:12:20 +0530

    New Inclusion

---
+++ Hardware-Assignment/codes/data.cpp
@@ -0,0 +1,19 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(1000);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  double sum=0;
+  int count=10;
+  while(count>0){
+    Serial.println((analogRead(A0)*5)/1023.0,3);
+    sum+=(analogRead(A0)*5)/1023.0;
+    count--;
+    delay(1000);
+  }
+    Serial.print("Average value: ");
+    Serial.println(sum/10,3);
+    delay(1000);
+}
